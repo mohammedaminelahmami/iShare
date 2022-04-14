28da954a5cff0e4fdb9952d51278cdd783b143a7
mohammedaminelahmami <lahmamimohammedamine@gmail.com>
2022-04-14 12:50:38 +0200

    first

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+    <div className='parentApp'>
+
+      {/* <nav>
+        <Link to='/'><button type='submit' className='p-4 bg-firstColor text-white'>Submit</button></Link>
+        <Link to='/profile'><button type='submit' className='p-4 bg-secondColor text-white'>Submit</button></Link>
+      </nav> */}
+
+      <Switch>
+
+        <Route exact path='/'>
+          {/* <div> Hello World</div> */}
+        </Route>
+
+        <Route path='/profile'>
+          {/* <div> Hey Mohammed Amine Lahmami </div> */}
+        </Route>
+
+      </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        firstColor : '#5463FF',
+        secondColor : '#261D8E',
+        thirdColor : '#113C5E',
+      },
+    },
+  },
+  plugins: [],
+}
